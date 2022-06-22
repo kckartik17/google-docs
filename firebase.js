3fbeec8e217cccfeb81bfa5eb6bec994b088eb8b
Kartik Chauhan <kckartik17@gmail.com>
2022-06-22 21:29:01 +0530

    fixes
    Signed-off-by: Kartik Chauhan <kckartik17@gmail.com>

--- firebase.js
+++ firebase.js
@@ -2,12 +2,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCKFGS9vW5kCnWRVxe16lzaq6SJqkFif5U",
-  authDomain: "docs-b0f7e.firebaseapp.com",
-  projectId: "docs-b0f7e",
-  storageBucket: "docs-b0f7e.appspot.com",
-  messagingSenderId: "847501251036",
-  appId: "1:847501251036:web:c1f3f5b88fcf464ce5ff19",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
